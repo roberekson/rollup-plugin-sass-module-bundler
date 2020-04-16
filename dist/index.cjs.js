321f32fab5005ec511193051d6d365ac0185e6ec
Rob Erekson <roberekson@gmail.com>
2020-04-15 17:25:49 -0700

    Build dependency tree to transpile SCSS files in order

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -1,19 +1,22 @@
 'use strict';
 
 var pluginutils = require('@rollup/pluginutils');
-var path = require('path');
 
 const chalk = require('chalk');
-const sha1 = require('sha1');
+const md5 = require('md5');
 const sass = require('sass');
+const parseImports = require('parse-es6-imports');
+const path = require('path');
+const fs = require('fs');
 const defaultOptions = {
     include: [/\.s?(a|c)ss$/],
     exclude: [],
     includePaths: [],
+    sourceMap: null,
 };
-const stylesheets = new Map();
+const stylesheets = new Map;
 const hashLength = 8;
-const transpile = (scss, path, options) => {
+const transpile = (scss, key, options) => {
     let returnObj = {
         css: '',
         map: '',
@@ -23,19 +26,44 @@ const transpile = (scss, path, options) => {
     if (scss.length) {
         try {
             const result = sass.renderSync({
-                file: path,
+                file: key,
                 outFile: 'build/css/standaloneSiteFooter.css',
                 includePaths: options.includePaths,
+                sourceMap: false,
             });
-            returnObj = Object.assign(Object.assign({}, returnObj), { css: result.css.toString(), duration: result.stats.duration, size: Buffer.byteLength(result.css, 'utf8') });
+            returnObj = {
+                ...returnObj,
+                css: result.css ? result.css.toString() : '',
+                map: result.map ? result.map.toString() : null,
+                duration: result.stats.duration,
+                size: Buffer.byteLength(result.css, 'utf8'),
+            };
         }
         catch (e) {
-            outputError(e, path);
+            outputError(e, key);
         }
+    }
+    if (returnObj.css) {
+        stylesheets.get(key).transpiled = returnObj.css;
     }
     return returnObj;
 };
-const outputError = (e, path = '') => {
+const loadCss = (key, options) => {
+    let css = '';
+    if (isCssFile(key)) {
+        css = transpile(stylesheets.get(key).code, key, options).css;
+    }
+    if (stylesheets.has(key) && 'imports' in stylesheets.get(key)) {
+        stylesheets.get(key).imports.forEach(i => {
+            css += loadCss(i.path, options);
+        });
+    }
+    return css;
+};
+const isCssFile = (filename) => {
+    return filename.substr(-3) === 'css';
+};
+const outputError = (e, filepath = '') => {
     let message = '';
     let errorTitle = '';
     if (e.message.includes('expected')) {
@@ -48,8 +76,8 @@ const outputError = (e, path = '') => {
         errorTitle = 'ERROR';
     }
     message = `\n-- ${errorTitle} ${'-'.repeat(47 - errorTitle.length)}\n`;
-    if (e.file || path) {
-        message += `File: ${e.file !== 'stdin' ? e.file : path}\n`;
+    if (e.file || filepath) {
+        message += `File: ${e.file !== 'stdin' ? e.file : filepath}\n`;
     }
     if (e.line) {
         message += `Line: ${e.line}` + (e.column ? `:${e.column}\n` : '');
@@ -64,43 +92,93 @@ const outputError = (e, path = '') => {
     message += `${'-'.repeat(50)}\n`;
     console.log(chalk.bold.red(message));
 };
+const getJsImports = (code, absolutePath) => {
+    const imports = parseImports(code).map(i => {
+        const p = path.parse(i.fromModule);
+        const ext = p.ext ? p.ext.substr(1) : 'js';
+        if (['scss', 'sass', 'css', 'ts', 'js'].includes(ext)) {
+            const importPath = getRealPath(p.dir, absolutePath);
+            return {
+                name: p.name,
+                type: ext,
+                path: `${importPath}/${p.name}.${ext}`,
+            };
+        }
+        return false;
+    }).filter(i => i);
+    return imports;
+};
+/**
+ * Assumptions are being made here:
+ * - If the path starts with a '.', then we assume it's relative to the refPath and will be resolved
+ * - Otherwise assume it's located in the node_modules directory of the process's directory
+ *
+ * TO DO: Walk directory tree and find closest node_modules directory
+ */
+const getRealPath = (filepath, refPath) => {
+    let realPath = '';
+    if (filepath[0] === '.') {
+        realPath = path.resolve(path.dirname(refPath), filepath);
+    }
+    else {
+        realPath = `${process.cwd()}/node_modules/${filepath}`;
+    }
+    return fs.realpathSync(realPath);
+};
+const addModuleToTree = (name, imports, code) => {
+    const realName = fs.realpathSync(name);
+    if (!stylesheets.has(realName)) {
+        stylesheets.set(realName, {
+            code,
+            imports,
+        });
+    }
+};
 const createTransform = (filter) => (code, id) => {
+    const imports = getJsImports(code, id);
+    addModuleToTree(id, imports, code);
     if (!filter(id)) {
         return;
     }
-    stylesheets.set(id, code);
     return '';
 };
 const watchChange = (id) => {
     console.log('CHANGE');
 };
 const createGenerateBundle = (moduleOptions) => function (options, bundle) {
-    const path$1 = stylesheets.keys().next().value;
-    const bundleName = getBundleName(bundle) || path$1(path$1, path.extname(path$1));
-    let source = '';
     let duration = 0;
     let size = 0;
-    stylesheets.forEach((value, key) => {
-        const result = transpile(value, key, moduleOptions);
-        source += result.css;
-        duration += result.duration;
-        size += result.size;
-    });
+    const transpileOptions = {
+        ...moduleOptions,
+    };
+    if (!(moduleOptions === null) && options.sourcemap !== false) {
+        transpileOptions.sourceMap = true;
+    }
+    const entrypoint = stylesheets.keys().next().value;
+    const source = loadCss(entrypoint, transpileOptions);
+    const bundleName = getBundleName(bundle) || path.basename(entrypoint, path.extname(entrypoint));
+    const filename = buildFilename(bundleName, source, options);
     this.emitFile({
         source,
         type: 'asset',
-        name: bundleName,
-        fileName: buildFilename(bundleName, source, options),
+        fileName: filename,
     });
+    // this.emitFile({
+    //     source: sourceMap,
+    //     type: 'asset',
+    //     fileName: `${filename}.map`,
+    // });
     console.log(chalk.green(`${chalk.bold('Processed')}: ${bundleName}.css (${formatSize(size)}) in ${duration}ms\n`));
     return;
 };
 const buildFilename = (filename, contents, options) => {
+    const hash = md5(contents);
+    console.log(hash);
     let newFilename = options.assetFileNames
         .replace(/\[ext\]/g, 'css')
         .replace(/\[extname\]/g, '.css')
         .replace(/\[name\]/g, filename)
-        .replace(/\[hash\]/g, sha1(contents).substr(0, options.hashLength || hashLength));
+        .replace(/\[hash\]/g, hash.substr(0, options.hashLength || hashLength));
     return newFilename;
 };
 const formatSize = (size) => {
@@ -130,7 +208,10 @@ const getBundleName = (bundle) => {
     return '';
 };
 var index = (options = {}) => {
-    const moduleOptions = Object.assign(Object.assign({}, defaultOptions), options);
+    const moduleOptions = {
+        ...defaultOptions,
+        ...options
+    };
     const filter = pluginutils.createFilter(moduleOptions.include, moduleOptions.exclude);
     return {
         name: 'scss-module-bundler',
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,30 @@
 import { createFilter,  } from '@rollup/pluginutils';
-import { basename, extname } from 'path';
 
 const chalk = require('chalk');
-const sha1 = require('sha1');
+const md5 = require('md5');
 const sass = require('sass');
+const parseImports = require('parse-es6-imports');
+const path = require('path');
+const fs = require('fs');
 
 interface Options {
     include: string[] | RegExp[];
     exclude: string[];
     includePaths: string[];
+    sourceMap: boolean;
 }
 
-const defaultOptions = {
+const defaultOptions: Options = {
     include: [/\.s?(a|c)ss$/],
     exclude: [],
     includePaths: [],
+    sourceMap: null,
 };
 
-const stylesheets = new Map();
+const stylesheets = new Map;
 const hashLength = 8;
 
-const transpile = (scss, path, options) => {
+const transpile = (scss, key, options) => {
     let returnObj = {
         css: '',
         map: '',
@@ -31,27 +35,53 @@ const transpile = (scss, path, options) => {
     if (scss.length) {
         try {
             const result = sass.renderSync({
-                file: path,
+                file: key,
                 outFile: 'build/css/standaloneSiteFooter.css',
                 includePaths: options.includePaths,
+                sourceMap: false,
             });
 
             returnObj = {
                 ...returnObj,
-                css: result.css.toString(),
+                css: result.css ? result.css.toString() : '',
+                map: result.map ? result.map.toString() : null,
                 duration: result.stats.duration,
                 size: Buffer.byteLength(result.css, 'utf8'),
             };
 
         } catch (e) {
-            outputError(e, path);
+            outputError(e, key);
         }
     }
 
+    if (returnObj.css) {
+        stylesheets.get(key).transpiled = returnObj.css;
+    }
+    
     return returnObj;
 }
 
-const outputError = (e, path = ''): void => {
+const loadCss = (key: string, options) => {
+    let css = '';
+
+    if (isCssFile(key)) {
+        css = transpile(stylesheets.get(key).code, key, options).css;
+    }
+
+    if (stylesheets.has(key) && 'imports' in stylesheets.get(key)) {
+        stylesheets.get(key).imports.forEach(i => {
+            css += loadCss(i.path, options);
+        });
+    }
+
+    return css;
+}
+
+const isCssFile = (filename: string) => {
+    return filename.substr(-3) === 'css';
+}
+
+const outputError = (e, filepath = ''): void => {
     let message = '';
     let errorTitle = '';
 
@@ -65,8 +95,8 @@ const outputError = (e, path = ''): void => {
 
     message = `\n-- ${errorTitle} ${'-'.repeat(47 - errorTitle.length)}\n`;
     
-    if (e.file || path) {
-        message += `File: ${e.file !== 'stdin' ? e.file : path}\n`;
+    if (e.file || filepath) {
+        message += `File: ${e.file !== 'stdin' ? e.file : filepath}\n`;
     }
     
     if (e.line) {
@@ -86,12 +116,65 @@ const outputError = (e, path = ''): void => {
     console.log(chalk.bold.red(message));
 }
 
+const getJsImports = (code: string, absolutePath: string): [] => {
+    const imports = parseImports(code).map(i => {
+        const p = path.parse(i.fromModule);
+        const ext = p.ext ? p.ext.substr(1) : 'js';
+
+        if (['scss', 'sass', 'css', 'ts', 'js'].includes(ext)) {
+            const importPath = getRealPath(p.dir, absolutePath)
+
+            return {
+                name: p.name,
+                type: ext,
+                path: `${importPath}/${p.name}.${ext}`,
+            };
+        }
+
+        return false;
+    }).filter(i => i);
+    
+    return imports;
+}
+
+/**
+ * Assumptions are being made here:
+ * - If the path starts with a '.', then we assume it's relative to the refPath and will be resolved
+ * - Otherwise assume it's located in the node_modules directory of the process's directory
+ * 
+ * TO DO: Walk directory tree and find closest node_modules directory
+ */
+const getRealPath = (filepath, refPath) => {
+    let realPath = '';
+
+    if (filepath[0] === '.') {
+        realPath = path.resolve(path.dirname(refPath), filepath);
+    
+    } else {
+        realPath = `${process.cwd()}/node_modules/${filepath}`;
+    }
+
+    return fs.realpathSync(realPath);
+};
+
+const addModuleToTree = (name: string, imports, code: string) => {
+    const realName = fs.realpathSync(name);
+    if (!stylesheets.has(realName)) {
+        stylesheets.set(realName, {
+            code,
+            imports,
+        });
+    }
+}
+
 const createTransform = (filter): Function => (code: string, id: string): string | void => {
+    const imports = getJsImports(code, id);
+
+    addModuleToTree(id, imports, code);
+
     if (!filter(id)) {
         return;
     }
-    
-    stylesheets.set(id, code);
 
     return '';
 }
@@ -101,39 +184,48 @@ const watchChange = (id: string): void => {
 }
 
 const createGenerateBundle = (moduleOptions: Options) => function (options: Record<string, any>, bundle) {
-    const path = stylesheets.keys().next().value;
-    const bundleName = getBundleName(bundle) || path(path, extname(path));
-
-    let source = '';
+    let sourceMap = '';
     let duration = 0;
     let size = 0;
 
-    stylesheets.forEach((value, key) => {
-        const result = transpile(value, key, moduleOptions);
-        
-        source += result.css;
-        duration += result.duration;
-        size += result.size;
-    });
+    const transpileOptions = {
+        ...moduleOptions,
+    };
+
+    if (!(moduleOptions === null) && options.sourcemap !== false) {
+        transpileOptions.sourceMap = true;
+    }
+    const entrypoint = stylesheets.keys().next().value;
+    const source = loadCss(entrypoint, transpileOptions);
+
+    const bundleName = getBundleName(bundle) || path.basename(entrypoint, path.extname(entrypoint));
+    const filename = buildFilename(bundleName, source, options);
 
     this.emitFile({
         source,
         type: 'asset',
-        name: bundleName,
-        fileName: buildFilename(bundleName, source, options),
+        fileName: filename,
     });
+    
+    // this.emitFile({
+    //     source: sourceMap,
+    //     type: 'asset',
+    //     fileName: `${filename}.map`,
+    // });
 
     console.log(chalk.green(`${chalk.bold('Processed')}: ${bundleName}.css (${formatSize(size)}) in ${duration}ms\n`));
 
     return;
 }
 
-const buildFilename = (filename: string, contents, options: Record<string, any>): string => {
+const buildFilename = (filename: string, contents: string, options: Record<string, any>): string => {
+    const hash = md5(contents);
+    console.log(hash);
     let newFilename = options.assetFileNames
         .replace(/\[ext\]/g, 'css')
         .replace(/\[extname\]/g, '.css')
         .replace(/\[name\]/g, filename)
-        .replace(/\[hash\]/g, sha1(contents).substr(0, options.hashLength || hashLength));
+        .replace(/\[hash\]/g, hash.substr(0, options.hashLength || hashLength));
 
     return newFilename;
 }
